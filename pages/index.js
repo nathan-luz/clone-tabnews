af1340d2ee1fb1224c9a4be39d22900683d5ecb8
NathanLuz <56982573+nathan-luz@users.noreply.github.com>
2024-04-24 17:21:13 +0000

    changing message

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Mozinho, amo você, em todos os lugares do mundo.</h1>
+    return <h1>Mozinho, amo você, em todos os lugares do mundo. Se você me ama, da uma risadinha rsrs </h1>
 };
 
 export default Home;
