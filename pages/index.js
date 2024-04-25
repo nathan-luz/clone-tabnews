d4d896e3e69d43e744beba8a6e944fc45e099d77
NathanLuz <56982573+nathan-luz@users.noreply.github.com>
2024-04-25 02:51:20 +0000

    tryna add some cool template

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,74 @@
 function Home() {
-  return <h1>Hello World, Leonardo!</h1>;
+  return (
+    <html lang="en">
+      <head>
+        <meta charset="UTF-8"></meta>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0"
+        ></meta>
+        <meta http-equiv="X-UA-Compatible" content="ie=edge"></meta>
+        <title>BootstrapDash Comingsoon Template</title>
+        <link
+          href="https://fonts.googleapis.com/css?family=Karla:400,700&display=swap"
+          rel="stylesheet"
+        ></link>
+        <link
+          rel="stylesheet"
+          href="https://cdn.materialdesignicons.com/4.8.95/css/materialdesignicons.min.css"
+        ></link>
+        <link
+          rel="stylesheet"
+          href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+        ></link>
+        <link rel="stylesheet" href="assets/css/bd-coming-soon.css"></link>
+      </head>
+      <body class="min-vh-100 d-flex flex-column">
+        <header>
+          <div class="container">
+            <nav class="navbar navbar-dark bg-transparenet">
+              <a class="navbar-brand" href="#">
+                <img src="assets/images/logo.svg" alt="logo"></img>
+              </a>
+              <span class="navbar-text ml-auto d-none d-sm-inline-block">
+                978-130-5444{" "}
+              </span>
+              <span class="navbar-text d-none d-sm-inline-block">
+                jerald_glover@glennie.tv
+              </span>
+            </nav>
+          </div>
+        </header>
+        <main class="my-auto">
+          <div class="container">
+            <h1 class="page-title">We're coming soon</h1>
+            <p class="page-description">
+              Working on finishing this development here.
+            </p>
+            <p>You can expect this amazing site running soon.</p>
+            <p>Stay connected</p>
+            <nav class="footer-social-links">
+              <a href="#!" class="social-link">
+                <i class="mdi mdi-facebook-box"></i>
+              </a>
+              <a href="#!" class="social-link">
+                <i class="mdi mdi-twitter"></i>
+              </a>
+              <a href="#!" class="social-link">
+                <i class="mdi mdi-google"></i>
+              </a>
+              <a href="#!" class="social-link">
+                <i class="mdi mdi-slack"></i>
+              </a>
+              <a href="#!" class="social-link">
+                <i class="mdi mdi-skype"></i>
+              </a>
+            </nav>
+          </div>
+        </main>
+      </body>
+    </html>
+  );
 }
 
 export default Home;
